6950aedc4209bab126dc5d38872be0ff6724120d
Gregg Abe <greggabe808@gmail.com>
2025-03-22 03:12:20 -1000

    Added files to project.

---
+++ WaterApp/WaterApp/App/WaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterAppApp.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
