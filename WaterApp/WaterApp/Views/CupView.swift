45878a291f1a6cb8c8790f4f82bb52b7e3cea765
Gregg Abe <greggabe808@gmail.com>
2025-03-22 03:58:47 -1000

    Added wave template to HomeView.

---
+++ WaterApp/WaterApp/Views/CupView.swift
@@ -0,0 +1,43 @@
+//
+//  CupView.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 3/22/25.
+//
+
+import SwiftUI
+
+struct CupView: View {
+    @State private var waveOffset = Angle(degrees: 0)
+    
+    let percent: Int
+    
+    var body: some View {
+        
+        GeometryReader { geo in
+            ZStack {
+                Text("\(self.percent)%")
+                    .foregroundColor(.black)
+                    .font(Font.system(size: 0.25 * min(geo.size.width, geo.size.height) ))
+                Circle()
+                    .stroke(Color.blue, lineWidth: 0.025 * min(geo.size.width, geo.size.height))
+                    .overlay(
+                        WaveMotion(offset: Angle(degrees: self.waveOffset.degrees), percent: Double(percent)/100)
+                            .fill(Color(red: 0, green: 0.5, blue: 0.75, opacity: 0.5))
+                            .clipShape(Circle().scale(0.92))
+                    )
+            }
+        }
+        
+        .aspectRatio(1, contentMode: .fit)
+        .onAppear {
+            withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                self.waveOffset = Angle(degrees: 360)
+            }
+        }
+    }
+}
+
+#Preview {
+    CupView(percent: 35)
+}
