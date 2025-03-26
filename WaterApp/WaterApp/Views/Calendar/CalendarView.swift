675180517e04fc281d8f343bb4fcb8805115f2c2
Gregg Abe <greggabe808@gmail.com>
2025-03-26 13:16:34 -1000

    Created CalendarView and organized files.

---
+++ WaterApp/WaterApp/Views/Calendar/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 3/26/25.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
