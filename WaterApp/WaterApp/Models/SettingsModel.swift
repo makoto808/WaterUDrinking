cd41e2d2c3b6a7a0bb80ad2cced8120a6b6f831f
Gregg Abe <greggabe808@gmail.com>
2025-03-27 23:15:44 -1000

    Added SettingsModel to project.

---
+++ WaterApp/WaterApp/Models/SettingsModel.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsModel.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 3/27/25.
+//
+
+import SwiftUI
+
+struct SettingsModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsModel()
+}
