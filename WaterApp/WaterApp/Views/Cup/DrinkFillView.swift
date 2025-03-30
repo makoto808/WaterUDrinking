7fb4c4330bebb8f6cfc3414def0897f74dc6acb7
Gregg Abe <greggabe808@gmail.com>
2025-03-29 18:02:26 -1000

    Design or DrinkFillView

--- WaterApp/WaterApp/Views/Cup/DrinkFillView.swift
+++ WaterApp/WaterApp/Views/Cup/DrinkFillView.swift
@@ -9,7 +9,52 @@ import SwiftUI
 
 struct DrinkFillView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Spacer()
+            
+            Image("waterBottle")
+                .resizable()
+                .frame(width: 500, height: 500, alignment: .center)
+            
+            Text("16.9 oz")
+                .font(.custom("ArialRoundedMTBold", size: 45))
+                
+            Spacer()
+            
+            HStack {
+                Button {
+                    
+                } label: {
+                    Image("waterBottle")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                }
+
+                Spacer()
+                
+                Button("+ WATER") {
+                    
+                }
+                .buttonBorderShape(.capsule)
+                .buttonStyle(.borderedProminent)
+               
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "ellipsis.circle")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                }
+            }
+            .padding(20)
+            
+            Spacer()
+            
+        }
     }
 }
 
