bbf87a66803d594bd570c25bf1433c3a87dbc577
Gregg Abe <greggabe808@gmail.com>
2025-03-29 17:40:53 -1000

    Added DrinkFillView to project.

---
+++ WaterApp/WaterApp/Views/Cup/DrinkFillView.swift
@@ -0,0 +1,18 @@
+//
+//  DrinkFillView.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 3/29/25.
+//
+
+import SwiftUI
+
+struct DrinkFillView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrinkFillView()
+}
