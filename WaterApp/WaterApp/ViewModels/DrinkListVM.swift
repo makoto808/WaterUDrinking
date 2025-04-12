b134137fcb0205b7daf4f22cc3a13f48f7f7a357
Ryan Kanno <ryantkanno@gmail.com>
2025-04-12 09:55:32 -0700

    Fix line break and indentation spacing.

--- WaterApp/WaterApp/ViewModels/DrinkListVM.swift
+++ WaterApp/WaterApp/ViewModels/DrinkListVM.swift
@@ -8,8 +8,8 @@
 import Foundation
 import Observation
 
-@Observable final class DrinkListVM: ObservableObject {
-/*    var path: [Int] = []*/ //ryan lesson 146
+@Observable final class DrinkListVM {
+    var navPath: [NavPath] = [] //ryan lesson 146
     
     var items: [DrinkItem] = [
         DrinkItem(name: "Water", img: "waterBottle", volume: 0.0),
@@ -20,10 +20,7 @@ import Observation
         DrinkItem(name: "Milk", img: "milk", volume: 0.0),
         DrinkItem(name: "Energy Drink", img: "energyDrink", volume: 0.0),
         DrinkItem(name: "Beer", img: "beer", volume: 0.0)
-    ]
-
-
-        {
+    ] {
         didSet {
             let sum = items.reduce(0.0) { $0 + ($1.volume + Double($1.volume))
             }
@@ -31,8 +28,7 @@ import Observation
         }
     }
     
-var totalOz: Double = 0.0
-    
+    var totalOz: Double = 0.0
 }
 
 //@State private var drinkImage: [String] = ["waterBottle", "tea", "coffee", "soda", "juice", "milk", "energyDrink", "beer"]
