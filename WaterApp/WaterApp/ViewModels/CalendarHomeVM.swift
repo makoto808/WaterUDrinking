c29778ebc1c2cca8604be031f9ffd238fc9067cc
Ryan Kanno <ryantkanno@gmail.com>
2025-04-19 01:01:33 -0700

    Add CalendarHomeVM to project.

---
+++ WaterApp/WaterApp/ViewModels/CalendarHomeVM.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarHomeVM.swift
+//  WaterApp
+//
+//  Created by Ryan Kanno on 4/19/25.
+//
+
+import Foundation
+import Observation
+
+@Observable final class CalendarHomeVM {
+    
+    
+    
+}
