9d4312ec6882c70eb80187ba234eca3a1ed6b5a3
Ryan Kanno <ryantkanno@gmail.com>
2025-04-19 00:50:40 -0700

    Add init to CachedDailyTotal.

--- WaterApp/WaterApp/Models/DrinkItem.swift
+++ WaterApp/WaterApp/Models/DrinkItem.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftData
 
+/// Use this to display in DrinkSelectionView
 struct DrinkItem: Identifiable, Hashable {
     var id: String
     var name: String
@@ -31,5 +32,11 @@ struct DrinkItem: Identifiable, Hashable {
         self.id = UUID().uuidString
         self.date = date
         self.volume = volume
+    }
+    
+    init(_ drinkItem: DrinkItem) {
+        self.id = drinkItem.id
+        self.date = .now
+        self.volume = drinkItem.volume
     }
 }
