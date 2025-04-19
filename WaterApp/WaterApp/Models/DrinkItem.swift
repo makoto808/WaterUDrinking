cc4d2deb6a9cb372fc1fe7fd8ee267dd002add3d
Ryan Kanno <ryantkanno@gmail.com>
2025-04-18 19:26:16 -0700

    Add CachedDailyTotal model to project.

--- WaterApp/WaterApp/Models/DrinkItem.swift
+++ WaterApp/WaterApp/Models/DrinkItem.swift
@@ -8,8 +8,7 @@
 import Foundation
 import SwiftData
 
-@Model
-class DrinkItem: Identifiable, Hashable {
+struct DrinkItem: Identifiable, Hashable {
     var id: String
     var name: String
     var img: String
@@ -19,6 +18,18 @@ class DrinkItem: Identifiable, Hashable {
         self.id = UUID().uuidString
         self.name = name
         self.img = img
+        self.volume = volume
+    }
+}
+
+@Model class CachedDailyTotal: Identifiable, Hashable {
+    var id: String
+    var date: Date
+    var volume: Double
+    
+    init(date: Date, volume: Double) {
+        self.id = UUID().uuidString
+        self.date = date
         self.volume = volume
     }
 }
