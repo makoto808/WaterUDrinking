988c4e0c2f7365fad0692f46d2b5077e3962b79b
Ryan Kanno <ryantkanno@gmail.com>
2025-04-19 01:23:13 -0700

    Fetch CachedDrinkItems from disc ModelContext.

--- WaterApp/WaterApp/ViewModels/CalendarHomeVM.swift
+++ WaterApp/WaterApp/ViewModels/CalendarHomeVM.swift
@@ -12,8 +12,23 @@ import SwiftData
 @Observable final class CalendarHomeVM {
     private var modelContext: ModelContext?
     
+    var items: [DrinkItem] = []
+    
     func setModelContext(_ modelContext: ModelContext) {
         self.modelContext = modelContext
         self.modelContext!.autosaveEnabled = true
+    }
+    
+    func getCachedItems(for date: Date) {
+        do {
+            let descriptor = FetchDescriptor<CachedDrinkItem>(
+                predicate: #Predicate { $0.date == date },
+                sortBy: [SortDescriptor(\.arrayOrderValue)])
+            if let cachedItems = try modelContext?.fetch(descriptor) {
+                items = cachedItems.map({ DrinkItem($0) })
+            }
+        } catch {
+            print(error)
+        }
     }
 }
