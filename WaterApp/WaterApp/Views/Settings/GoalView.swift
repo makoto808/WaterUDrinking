bb9dc3cd3ed9786eded967c6c28e966a84c866e9
Gregg Abe <greggabe808@gmail.com>
2025-06-11 19:20:46 -1000

    Added Text to GoalView

--- WaterApp/WaterApp/Views/Settings/GoalView.swift
+++ WaterApp/WaterApp/Views/Settings/GoalView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct GoalView: View {
+    @State private var dailyWaterGoal: String = ""
+    
+    @FocusState private var keyboardFocused: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.backgroundWhite
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("What is your daily water goal?")
+                    .font(.custom("ArialRoundedMTBold", size: 45))
+                    .foregroundStyle(.primary)
+                    .lineLimit(1)
+                    .allowsTightening(true)
+                    .minimumScaleFactor(0.30)
+                    .padding(.horizontal, 20)
+                
+                TextField("Enter Here", text: self.$dailyWaterGoal)
+                    .font(.custom("ArialRoundedMTBold", size: 40))
+                    .foregroundStyle(.primary)
+                    .lineLimit(1)
+                    .focused($keyboardFocused)
+                    .keyboardType(.numberPad)
+                    .multilineTextAlignment(.center) // Text aligns center
+                    .frame(width: 300) // Control width to allow centering
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+        }
     }
 }
 
