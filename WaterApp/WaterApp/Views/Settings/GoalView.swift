b951ed7c34d1159ab4a9fbaf28ed581826855b7d
Gregg Abe <greggabe808@gmail.com>
2025-06-11 18:48:52 -1000

    Added GoalView to SettingsView

---
+++ WaterApp/WaterApp/Views/Settings/GoalView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalView.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 6/11/25.
+//
+
+import SwiftUI
+
+struct GoalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalView()
+}
