ebdc2ebdf641fc4350a1b808b80fea994eb29a6a
Gregg Abe <greggabe808@gmail.com>
2025-06-14 15:08:02 -1000

    Fixed design UI for GoalView

--- WaterApp/WaterApp/Views/Settings/GoalView.swift
+++ WaterApp/WaterApp/Views/Settings/GoalView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct GoalView: View {
-    @Environment(DrinkListVM.self) private var vm
+        @Environment(DrinkListVM.self) private var vm
     
     @State private var dailyWaterGoal: String = ""
     @State private var waveOffset = Angle(degrees: 0)
@@ -20,7 +20,7 @@ struct GoalView: View {
             Color.backgroundWhite
                 .ignoresSafeArea()
             
-            WaveMotion(offset: Angle(degrees: self.waveOffset.degrees), percent: Double(3.0/8.0))
+            WaveMotion(offset: Angle(degrees: self.waveOffset.degrees), percent: Double(3.9/8.0))
                 .fill(Color(red: 0, green: 0.5, blue: 0.75, opacity: 0.5))
                 .ignoresSafeArea()
                 .onAppear {
@@ -32,7 +32,7 @@ struct GoalView: View {
             VStack {
                 Spacer()
                 
-                Text("What is your daily water oz goal?")
+                Text("Daily Water Goal")
                     .font(.custom("ArialRoundedMTBold", size: 45))
                     .foregroundStyle(.primary)
                     .lineLimit(1)
@@ -40,15 +40,18 @@ struct GoalView: View {
                     .minimumScaleFactor(0.30)
                     .padding(.horizontal, 5)
                 
-                TextField("Enter Here", text: self.$dailyWaterGoal)
-                    .font(.custom("ArialRoundedMTBold", size: 35))
-                    .foregroundStyle(.primary)
-                    .lineLimit(1)
-                    .focused($keyboardFocused)
-                    .keyboardType(.numberPad)
-                    .multilineTextAlignment(.center) // Text aligns center
-                    .frame(width: 300) // Control width to allow centering
+                HStack {
+                    TextField("Enter Here", text: (self.$dailyWaterGoal))
+                        .font(.custom("ArialRoundedMTBold", size: 35))
+                        .foregroundStyle(.primary)
+                        .lineLimit(1)
+                        .focused($keyboardFocused)
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.center) //Aligns text to center
+                }
                 
+                Spacer()
+                Spacer()
                 Spacer()
                 
                 if dailyWaterGoal.isEmpty {
@@ -56,6 +59,7 @@ struct GoalView: View {
                 } else {
                     Button("Right On!") {
                         // Add logic to save to oz goal
+                        keyboardFocused = false
                     }
                     .buttonBorderShape(.capsule)
                     .buttonStyle(.borderedProminent)
--- WaterApp/WaterApp/Views/Settings/SettingsListView.swift
+++ WaterApp/WaterApp/Views/Settings/SettingsListView.swift
@@ -10,82 +10,81 @@ import SwiftUI
 struct SettingsListView: View {
     @Environment(DrinkListVM.self) private var vm
     
-    let backgroundWhite = Color(red: 0.9373, green: 0.9607, blue: 0.9607)
-    
     var body: some View {
-        ZStack {
-            Color.backgroundWhite.ignoresSafeArea()
-            
-            
-            VStack {
-                List {
-                    Section {
-                        Button("Give us a review") {
-
-                        }
+        VStack {
+            List {
+                Section {
+                    Button("Give us a review") {
+                        //TODO: Link to appstore review
                     }
-                    
-                    Section {
-                        Button {
-                            vm.navPath.append(.dailyWaterGoal)
-                        } label: {
-                            Text("Edit Your Goal")
-                        }
-                        Button("Set a reminder") {
-                            
-                        }
+                }
+                
+                Section {
+                    Button {
+                        vm.navPath.append(.dailyWaterGoal)
+                    } label: {
+                        Text("Edit Your Goal")
                     }
-                    
-                    Section {
-                        Button("Cocktail Menu") {
-                            
-                        }
-                        Button("Widgets") {
-                            
-                        }
-                        Button("App Icons") {
-                            
-                        }
-                    }
-                    
-                    Section {
-                        Button("Measurements") {
-                            
-                        }
-                        Button("Apple Health Sync") {
-                            
-                        }
-                        Button("Apple WatchOS") {
-                            
-                        }
-                        Button("iCloud Backup") {
-                            
-                        }
-                    }
-                    
-                    Section {
-                        Button("Language") {
-                            
-                        }
-                        Button("Help & Support") {
-                            
-                        }
-                        Button("Have an App Idea?") {
-                            
-                        }
-                    }
-                    
-                    Section {
-                        Button("Hydrate your friends!") {
-                            
-                        }
-                        .buttonBorderShape(.capsule)
-                        .buttonStyle(.borderedProminent)
-                    }
-                } 
-                .listStyle(.automatic)
+                }
+                
+                //TODO: Future Settings Tabs below
+                
+                //Button("Set a reminder") {
+                //
+                //                        }
+                //                    }
+                //
+                //                    Section {
+                //                        Button("Cocktail Menu") {
+                //
+                //                        }
+                //                        Button("Widgets") {
+                //
+                //                        }
+                //                        Button("App Icons") {
+                //
+                //                        }
+                //                    }
+                //
+                //                    Section {
+                //                        Button("Measurements") {
+                //
+                //                        }
+                //                        Button("Apple Health Sync") {
+                //
+                //                        }
+                //                        Button("Apple WatchOS") {
+                //
+                //                        }
+                //                        Button("iCloud Backup") {
+                //
+                //                        }
+                //                    }
+                //
+                //                    Section {
+                //                        Button("Language") {
+                //
+                //                        }
+                //                        Button("Help & Support") {
+                //
+                //                        }
+                //                        Button("Have an App Idea?") {
+                //
+                //                        }
+                //                    }
+                //
+                //                    Section {
+                //                        Button("Hydrate your friends!") {
+                //
+                //                        }
+                //                        .buttonBorderShape(.capsule)
+                //                        .buttonStyle(.borderedProminent)
+                //                    }
                 
             }
+            .listStyle(.automatic) //Lets swift choose appropriate list view based on device
+            .scrollContentBackground(.hidden)
+            .background(Color.backgroundWhite)
         }
     }
 }
