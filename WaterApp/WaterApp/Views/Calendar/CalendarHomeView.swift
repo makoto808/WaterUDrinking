6093b99d81e0c94c3a50285bb08b01b68d744e88
Gregg Abe <greggabe808@gmail.com>
2025-06-16 19:17:04 -1000

    Added ResetView and fixed constraints to CustomOzView and button for CalendarHomeView.

--- WaterApp/WaterApp/Views/Calendar/CalendarHomeView.swift
+++ WaterApp/WaterApp/Views/Calendar/CalendarHomeView.swift
@@ -8,15 +8,41 @@
 import SwiftUI
 
 struct CalendarHomeView: View { //TODO: Access water streak (Apple Fitness rings style)
-    @Environment(CalendarHomeVM.self) private var vm
+    @Environment(CalendarHomeVM.self) private var calendarHomeVM
+    
+    @Environment(DrinkListVM.self) private var vm
+    @State private var showAlert = false
     
     var body: some View {
         ZStack {
             Color.backgroundWhite.ignoresSafeArea()
             
             VStack {
+                Spacer()
+                
                 BarChart()
                 
+                Spacer()
+
+//                better to implemet reset button here instead?
+                    Button("Reset?") {
+                        showAlert = true
+                    }
+                    .buttonBorderShape(.capsule)
+                    .buttonStyle(.borderedProminent)
+                    .font(.custom("ArialRoundedMTBold", size: 25))
+                    .textCase(.uppercase)
+                    .alert("Are You Sure?", isPresented: $showAlert) {
+                        Button("Cancel", role: .cancel) {}
+                        Button("OK", role: .destructive) {
+                            vm.totalOz = 0
+                            vm.navPath.removeLast()
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                
+                Spacer()
 //                CalendarView()
             }
             .padding(.horizontal)
@@ -24,6 +50,11 @@ struct CalendarHomeView: View { //TODO: Access water streak (Apple Fitness rings
     }
 }
 
-#Preview {
-    CalendarHomeView()
-}
+//#Preview {
+//    let mockDrinkListVM = DrinkListVM()
+//    let mockCalendarHomeVM = CalendarHomeVM()
+//
+//    return CalendarHomeView()
+//        .environment(mockDrinkListVM)
+//        .environment(mockCalendarHomeVM)
+//}
