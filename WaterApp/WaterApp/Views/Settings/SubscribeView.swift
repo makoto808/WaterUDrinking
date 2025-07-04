6d55a69de07a2330982207d0906bac58a78a4408
Gregg Abe <greggabe808@gmail.com>
2025-07-03 21:53:39 -1000

    SubscribeView

---
+++ WaterApp/WaterApp/Views/Settings/SubscribeView.swift
@@ -0,0 +1,18 @@
+//
+//  SubscribeView.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 7/3/25.
+//
+
+import SwiftUI
+
+struct SubscribeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubscribeView()
+}
