395a02c3da697da6a0cecdd2b0220f85da87cab3
Gregg Abe <greggabe808@gmail.com>
2025-07-06 01:39:37 -1000

    NotificationView

---
+++ WaterApp/WaterApp/Views/Notification/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 7/6/25.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
