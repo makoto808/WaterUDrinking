17012c81f3834718945f23aa5d8a11a00f034fe8
Gregg Abe <greggabe808@gmail.com>
2025-07-19 18:53:52 -1000

    PremiumButtonToggle

---
+++ WaterApp/WaterApp/Models/PremiumButtonToggle.swift
@@ -0,0 +1,18 @@
+//
+//  PremiumButtonToggle.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 7/19/25.
+//
+
+import SwiftUI
+
+struct PremiumButtonToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PremiumButtonToggle()
+}
