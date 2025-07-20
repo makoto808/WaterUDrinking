9156117656b79ae0a161a7b622a42ce28018f3df
Gregg Abe <greggabe808@gmail.com>
2025-07-19 16:56:51 -1000

    Added file for AppColorScheme

---
+++ WaterApp/WaterApp/Models/AppColorScheme.swift
@@ -0,0 +1,18 @@
+//
+//  AppColorScheme.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 7/19/25.
+//
+
+import SwiftUI
+
+struct AppColorScheme: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppColorScheme()
+}
