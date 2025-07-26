3aa4b3fa723d662501d5a75568175b2d1c79a084
Gregg Abe <greggabe808@gmail.com>
2025-07-26 02:05:44 -1000

    Added UserArrangedDrinkItem

---
+++ WaterApp/WaterApp/Models/UserArrangedDrinkItem.swift
@@ -0,0 +1,31 @@
+//
+//  UserArrangedDrinkItem.swift
+//  WaterApp
+//
+//  Created by Gregg Abe on 7/26/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class UserArrangedDrinkItem: Identifiable, Hashable {
+    var id: String = UUID().uuidString
+    var name: String
+    var img: String
+    var arrayOrderValue: Int
+
+    init(name: String, img: String, arrayOrderValue: Int) {
+        self.name = name
+        self.img = img
+        self.arrayOrderValue = arrayOrderValue
+    }
+
+    static func == (lhs: UserArrangedDrinkItem, rhs: UserArrangedDrinkItem) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
