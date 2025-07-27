69f86fafe5f0f0bf29ef77fa888f405c6c3ef333
Gregg Abe <greggabe808@gmail.com>
2025-07-26 19:41:50 -1000

    Made row background lighter during dark mode

--- WaterApp/WaterApp/Models/DrinkMenuModel.swift
+++ WaterApp/WaterApp/Models/DrinkMenuModel.swift
@@ -10,13 +10,13 @@ import SwiftData
 
 struct DrinkMenuModel: View {
     @Environment(\.colorScheme) private var colorScheme
-
+    
     let drink: DrinkItem
     var isBeingDragged: Bool = false
-
+    
     var onDragStart: (() -> Void)? = nil
     var onDragEnd: (() -> Void)? = nil
-
+    
     var body: some View {
         Button(action: {
             // Your button action here
@@ -27,12 +27,12 @@ struct DrinkMenuModel: View {
                     .frame(width: 45, height: 45)
                     .padding(.leading, 15)
                     .padding(.trailing, 10)
-
+                
                 Text(drink.name)
                     .fontSmallTitle()
-
+                
                 Spacer()
-
+                
                 Image(systemName: "line.3.horizontal")
                     .foregroundStyle(.secondary)
                     .padding(.trailing, 15)
@@ -53,12 +53,14 @@ struct DrinkMenuModel: View {
                                 onDragEnd?()
                             }
                     )
-
+                
             }
             .contentShape(Rectangle())
             .frame(height: 65)
             .background(
-                Color.black.opacity(colorScheme == .dark ? 0.15 : 0.07)
+                colorScheme == .dark
+                ? Color.white.opacity(0.08)
+                : Color.black.opacity(0.07)
             )
             .cornerRadius(13)
             .padding(.vertical, 3)
@@ -74,9 +76,8 @@ struct DrinkMenuModel: View {
         drink: DrinkItem(name: "Water", img: "waterBottle", volume: 8.0),
         isBeingDragged: false
     )
-    .previewLayout(.sizeThatFits)
     .padding()
     .background(Color(.systemBackground)) // Match system background
-    .environment(\.colorScheme, .light) // Change to .dark to preview dark mode
+    .environment(\.colorScheme, .dark) // Change to .dark to preview dark mode
 }
 
