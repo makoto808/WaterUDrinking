61bc90cf0c147f962729d6f5f8d6e21dcbf57ea5
Gregg Abe <greggabe808@gmail.com>
2025-07-30 13:01:33 -1000

    Made edits to gearIcon size on HomeView

--- WaterApp/WaterApp/Views/Cup/HomeView.swift
+++ WaterApp/WaterApp/Views/Cup/HomeView.swift
@@ -54,7 +54,7 @@ struct HomeView: View {
                     Image("gearIcon")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30)
+                        .frame(width: 25, height: 25)
                         .padding(.horizontal, 6)
                         .padding(.top, 6)
                 }
